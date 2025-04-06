7cc1ef8d5c5bc95b44463102cfc80642978dfccd
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-06 19:40:12 +0300

    adding meal's schema and model

--- src/models/meal.model.ts
+++ src/models/meal.model.ts
@@ -1,10 +1,22 @@
+import { Schema, model } from "mongoose";
+
 export interface Meal {
     _id?: string;
     userId: string;
     name: string;
     category: "before workout" | "after workout" | "recovery" | "morning" | "other";
-    ingredients: string [];
+    ingredients: string[];
     description?: string;
     createdAt?: Date;
     updatedAt?: Date;
 }
+
+const MealSchema: Schema<Meal> = new Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    category: { type: String, enum: ["before workout", "after workout", "recovery", "morning", "other"], required: true },
+    ingredients: { type: [String], required: true },
+    description: { type: String },
+}, { timestamps: true });
+
+export const MealModel = model("Meal", MealSchema);
