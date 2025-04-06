7075f0cd709276dc4173e618921910b84d1b84b8
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-06 19:47:27 +0300

    adding workout schema and model

--- src/models/workout.model.ts
+++ src/models/workout.model.ts
@@ -1,3 +1,5 @@
+import { Schema, model } from "mongoose";
+
 export interface Workout {
     _id?: string;
     userId: string;
@@ -9,3 +11,14 @@ export interface Workout {
     createdAt?: Date;
     updatedAt?: Date;
 }
+
+const WorkoutSchema = new Schema({
+    userId: { type: String, required: true }, 
+    name: { type: String, required: true },
+    category: { type: String, enum: ["strength", "cardio", "flexibility", "other"], required: true },
+    duration: { type: Number },
+    intensity: { type: String, enum: ["low", "meduim", "high"] },
+    description: {type: String }, 
+}, { timestamps: true });
+
+export const WorkoutModel = model("Workout", WorkoutSchema);
