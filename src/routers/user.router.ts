ead5fe6a0f55d591d076e5e3c588f27b58cb0e73
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-07 14:01:38 +0300

    adding ge by id for user

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { UserModel } from "../models/user.model";
+import { Types } from "mongoose";
 
 export const userRouter = Router();
 
@@ -8,7 +9,7 @@ userRouter.get("/", async (_req: Request, res: Response) => {
         const users = await UserModel.find();
         res.json(users);
     } catch (error) {
-        res.status(500).json({ message: "faild to fetch users", error });
+        res.status(500).json({ message: "Faild to fetch users", error });
     }
 
 });
@@ -19,6 +20,28 @@ userRouter.post("/", async (req: Request, res: Response) => {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
     } catch (error) {
-        res.status(400).json({ message: "failed to create a user", error });
+        res.status(400).json({ message: "Failed to create a user", error });
+    }
+});
+
+userRouter.get("/:id", async (req:Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!Types.ObjectId.isValid(id)) {
+        res.status(400).json({ message: "Invalid user id format" });
+        return;
+    }
+
+    try {
+        const user = await UserModel.findById(id);
+        
+        if (!user) {
+            res.status(404).json({ message: "User was not found" });
+            return;
+        }
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Faild to fetch user", error });
     }
 });
