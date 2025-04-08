ab5f52597bb542264c1f7affbf40b173b15142e6
Coral Tzubery <tzubery.coral@gmail.com>
2025-04-08 14:00:26 +0300

    deviding the index.ts to other files

--- public/index.ts
+++ public/index.ts
@@ -1,58 +1,12 @@
-type Match = {
-    _id: string;
-    rating?: number;
-    comment?: string;
-    meal: { _id: string; name: string } | null;
-    workout: { _id: string; name: string } | null;
- }
+import { initMatches } from "./matches";
 
 export async function initApp(root: HTMLPreElement) {
-    root.textContent = "";
+    const container = document.createElement("div");
 
-    const button = document.createElement("button");
-    button.textContent = "Load Matches";
-    button.className = "load-btn";
+    const loadMatchedBtn = document.createElement("button");
+    loadMatchedBtn.textContent =  "Load Matches";
+    loadMatchedBtn.className = "load-btn";
+    loadMatchedBtn.onclick = () => initMatches(container);
 
-    const list = document.createElement("ul");
-    list.id = "matches-list";
-
-    button.addEventListener("click", async () => {
-        try {
-            const res = await fetch("/api/matches", { credentials: "include" });
-
-            if (!res.ok) {
-                throw new Error("Faild to fetch matches");
-            }
-
-            const matches: Match[] = await res.json();
-            list.innerHTML = "";
-
-            if (matches.length === 0 ) {
-                const noMatches = document.createElement("li");
-                noMatches.textContent = "No matches found";
-                list.appendChild(noMatches);
-                return;
-            }
-
-            matches.forEach((match) => {
-                const meal = match.meal?.name ?? "Unknown meal";
-                const workout = match.workout?.name ?? "Unknown workout";
-                const item = document.createElement("li");
-                
-                item.innerHTML = `
-                <strong>${meal}</strong> + <strong>${workout}</strong></br>
-                Rating: ${match.rating ?? "-"}</br>
-                <em>${match.comment ?? ""}</em>
-                `;
-                
-                list.appendChild(item);
-            });
-        } catch (error) {
-            list.innerHTML = "<li class = 'error'>Error loading matches</li>";
-            console.error(error);
-        }
-    });
-
-    root.appendChild(button);
-    root.appendChild(list);
+    root.replaceChildren(loadMatchedBtn, container);
 }
